b637f5576b56b350864e7a3e2e639537bc375229
Patrick Dubroy <pdubroy@gmail.com>
2024-03-24 12:19:42 +0100

    Share the memo table. + var → const/let

--- src/tree-walk.ts
+++ src/tree-walk.ts
@@ -1,7 +1,5 @@
 import { CstNode, Result } from "./types.js";
-import { MemoEntry } from "./MemoTable.js";
-
-type PosInfo = Map<string, MemoEntry>;
+import { MemoTable } from "./MemoTable.js";
 
 export interface PExpr {
   eval(matcher: Matcher): CstNode;
@@ -10,15 +8,15 @@ export interface PExpr {
 class Matcher {
   input: string;
   pos: number;
-  memoTable: PosInfo[];
+  memoTable: MemoTable<string>;
 
   constructor(public rules: { [k: string]: PExpr }) {}
 
   match(input: string) {
     this.input = input;
     this.pos = 0;
-    this.memoTable = [];
-    var cst = new RuleApplication("start").eval(this);
+    this.memoTable = new MemoTable();
+    const cst = new RuleApplication("start").eval(this);
     if (this.pos === this.input.length) {
       return cst;
     }
@@ -26,28 +24,18 @@ class Matcher {
   }
 
   hasMemoizedResult(ruleName: string) {
-    var col = this.memoTable[this.pos];
-    return col && col.has(ruleName);
+    return this.memoTable.has(this.pos, ruleName);
   }
 
   memoizeResult(pos: number, ruleName: string, cst: CstNode) {
-    var col = this.memoTable[pos];
-    if (!col) {
-      col = this.memoTable[pos] = new Map();
-    }
-    if (cst !== null) {
-      col.set(ruleName, {
-        cst: cst,
-        nextPos: this.pos,
-      });
-    } else {
-      col.set(ruleName, { cst: null });
-    }
+    this.memoTable.memoizeResult(pos, ruleName, {
+      cst,
+      nextPos: cst ? this.pos : undefined,
+    });
   }
 
   useMemoizedResult(ruleName: string) {
-    var col = this.memoTable[this.pos];
-    var result = col.get(ruleName);
+    const result = this.memoTable.getResult(this.pos, ruleName);
     if (result.cst !== null) {
       this.pos = result.nextPos;
       return result.cst;
@@ -68,12 +56,12 @@ export class RuleApplication {
   constructor(private ruleName: string) {}
 
   eval(matcher: Matcher): Result {
-    var name = this.ruleName;
+    const name = this.ruleName;
     if (matcher.hasMemoizedResult(name)) {
       return matcher.useMemoizedResult(name);
     } else {
-      var origPos = matcher.pos;
-      var cst = matcher.rules[name].eval(matcher);
+      const origPos = matcher.pos;
+      const cst = matcher.rules[name].eval(matcher);
       matcher.memoizeResult(origPos, name, cst);
       return cst;
     }
@@ -84,7 +72,7 @@ class Terminal {
   constructor(public str: string) {}
 
   eval(matcher: Matcher): Result {
-    for (var i = 0; i < this.str.length; i++) {
+    for (let i = 0; i < this.str.length; i++) {
       if (!matcher.consume(this.str[i])) {
         return null;
       }
@@ -114,10 +102,10 @@ class Choice {
   constructor(public exps: PExpr[]) {}
 
   eval(matcher: Matcher): Result {
-    var origPos = matcher.pos;
-    for (var i = 0; i < this.exps.length; i++) {
+    const origPos = matcher.pos;
+    for (let i = 0; i < this.exps.length; i++) {
       matcher.pos = origPos;
-      var cst = this.exps[i].eval(matcher);
+      const cst = this.exps[i].eval(matcher);
       if (cst !== null) {
         return cst;
       }
@@ -132,10 +120,10 @@ class Sequence {
   }
 
   eval(matcher: Matcher): Result {
-    var ans = [];
-    for (var i = 0; i < this.exps.length; i++) {
-      var exp = this.exps[i];
-      var cst = exp.eval(matcher);
+    const ans = [];
+    for (let i = 0; i < this.exps.length; i++) {
+      const exp = this.exps[i];
+      const cst = exp.eval(matcher);
       if (cst === null) {
         return null;
       }
@@ -151,7 +139,7 @@ class Not {
   constructor(public exp: PExpr) {}
 
   eval(matcher: Matcher): Result {
-    var origPos = matcher.pos;
+    const origPos = matcher.pos;
     if (this.exp.eval(matcher) === null) {
       matcher.pos = origPos;
       return [];
@@ -164,10 +152,10 @@ export class Repetition {
   constructor(public exp: PExpr) {}
 
   eval(matcher: Matcher): Result {
-    var ans = [];
+    const ans = [];
     while (true) {
-      var origPos = matcher.pos;
-      var cst = this.exp.eval(matcher);
+      const origPos = matcher.pos;
+      const cst = this.exp.eval(matcher);
       if (cst === null) {
         matcher.pos = origPos;
         break;
